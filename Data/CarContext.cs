1afdb7897e032f9f4a104d9538c59e687b519579
Bogjenero <bornagjenero1@gmail.com>
2023-11-13 21:16:58 +0100

    Add project files.

---
+++ Data/CarContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Car.Models;
+
+namespace Car.Data
+{
+    public class CarContext : DbContext
+    {
+        public CarContext (DbContextOptions<CarContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Car.Models.CarModel> Car { get; set; } = default!;
+    }
+}
---
+++ Models/CarModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Car.Models
+{
+    public class CarModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty; 
+        public string ProductionYear { get;set; } 
+        public decimal Price { get; set; }
+        public string Manufacturer { get; set; } = string.Empty;
+    }
+}
